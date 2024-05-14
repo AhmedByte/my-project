07bf89ec061895a63445363df4668d1f079d89ae
Ahmed <ahmed.hamadaabdrabo@gmail.com>
2024-05-14 03:03:53 +0300

    add function 3

--- gitTest.py
+++ gitTest.py
@@ -7,6 +7,13 @@ def fun2(n):
     for i in range(n):
         print(f"{i+1}.  Welcome")
 
-
+def fun3(n):
+    for i in range (n):
+        if (i%2==0):
+            status = 'even'
+        else:
+            status = 'odd'
+        print(f"{i} -> i'm {status}")
 fun1("ahmed")
 fun2(3)
+fun3(9)
